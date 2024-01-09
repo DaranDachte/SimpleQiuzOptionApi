c5ea4f4c2945ccb6b9bc5f2dcb53245c8db76c40
Daran Dachte <darandachte@gmail.com>
2024-01-09 16:47:04 +0100

    finish

---
+++ src/components/Result.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="result">
+    <div class="title">{{ results[resultIndex].title }}</div>
+    <div class="desc">{{ results[resultIndex].desc }}</div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  props: ['results', 'totalCorrect'],
+  computed: {
+    resultIndex() {
+      let index = 0
+
+      this.results.forEach((e: { min: number; max: number }, i: number) => {
+        if (e.min <= this.totalCorrect && e.max >= this.totalCorrect) {
+          index = i
+        }
+      })
+      return index
+    }
+  }
+}
+</script>
+
+<style scoped></style>
